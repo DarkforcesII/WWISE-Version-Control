fd0d12eb3ec16aa3301a8cfe4f84cf454f71ceaf
DarkforcesII <darkforcesii@gmail.com>
2020-04-22 16:00:54 -0700

    Added WWISE GitIgnore

---
+++ Assets/AKPostEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AKPostEvent : MonoBehaviour
+{
+
+    [SerializeField] AK.Wwise.Event Music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Music.Post(gameObject);
+        print("it works");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
